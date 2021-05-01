83215c7d884fb8f2fd89dac8cf20433ce27cf4ba
赖力 <2454168525@qq.com>
2021-05-01 17:35:04 +0800

    add .gitignore and package stuffs

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='celery-scheduler',
+    version='dev',
+    packages=['celery_scheduler',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
+
